d2690607389fc1f3241ad23b2e0e3ed0edfc5174
halakaraki <halak@uber.com>
2018-07-31 14:41:27 -0700

    expand HelloSignal example to send multiple signals (#1)
    * expand HelloSignal example to send multiple signals

--- src/test/java/com/uber/cadence/samples/hello/HelloSignalTest.java
+++ src/test/java/com/uber/cadence/samples/hello/HelloSignalTest.java
@@ -25,6 +25,9 @@ import com.uber.cadence.samples.hello.HelloSignal.GreetingWorkflow;
 import com.uber.cadence.testing.TestWorkflowEnvironment;
 import com.uber.cadence.worker.Worker;
 import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -68,8 +71,8 @@ public class HelloSignalTest {
     testEnv.close();
   }
 
-  @Test(timeout = 5000)
-  public void testSignal() {
+  @Test//(timeout = 5000)
+  public void testSignal() throws Exception {
     // Get a workflow stub using the same task list the worker uses.
     WorkflowOptions workflowOptions =
         new WorkflowOptions.Builder()
@@ -80,21 +83,25 @@ public class HelloSignalTest {
         workflowClient.newWorkflowStub(GreetingWorkflow.class, workflowOptions);
 
     // Start workflow asynchronously to not use another thread to signal.
-    WorkflowClient.start(workflow::getGreeting);
+    WorkflowClient.start(workflow::getGreetings);
 
     // After start for getGreeting returns, the workflow is guaranteed to be started.
     // So we can send a signal to it using workflow stub immediately.
     // But just to demonstrate the unit testing of a long running workflow adding a long sleep here.
     testEnv.sleep(Duration.ofDays(1));
+    // This workflow keeps receiving signals until exit is called
     workflow.waitForName("World");
+    workflow.waitForName("Universe");
+    workflow.exit();
     // Calling synchronous getGreeting after workflow has started reconnects to the existing
     // workflow and
     // blocks until result is available. Note that this behavior assumes that WorkflowOptions are
     // not configured
     // with WorkflowIdReusePolicy.AllowDuplicate. In that case the call would fail with
     // WorkflowExecutionAlreadyStartedException.
-    String greeting = workflow.getGreeting();
-
-    assertEquals("Hello World!", greeting);
+    List<String> greetings = workflow.getGreetings();
+    assertEquals(2, greetings.size());
+    assertEquals("Hello World!", greetings.get(0));
+    assertEquals("Hello Universe!", greetings.get(1));
   }
 }
