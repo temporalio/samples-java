2bf422465f8fa40fb9197c90bb7749b7de6ecd3b
Maxim Fateev <mfateev@users.noreply.github.com>
2020-05-28 17:37:51 -0700

    Added updatabletimer sample (#15)

---
+++ src/main/java/io/temporal/samples/updatabletimer/UpdatableTimer.java
@@ -0,0 +1,56 @@
+/*
+ *  Copyright (c) 2020 Temporal Technologies, Inc. All Rights Reserved
+ *
+ *  Copyright 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ *  use this file except in compliance with the License. A copy of the License is
+ *  located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ *  or in the "license" file accompanying this file. This file is distributed on
+ *  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+
+package io.temporal.samples.updatabletimer;
+
+import io.temporal.workflow.Workflow;
+import java.time.Duration;
+import java.util.Date;
+import org.slf4j.Logger;
+
+public final class UpdatableTimer {
+
+  private final Logger logger = Workflow.getLogger(DynamicSleepWorkflowImpl.class);
+
+  private long wakeUpTime;
+  private boolean wakeUpTimeUpdated;
+
+  public void sleepUntil(long wakeUpTime) {
+    logger.info("sleepUntil: " + new Date(wakeUpTime));
+    this.wakeUpTime = wakeUpTime;
+    do {
+      wakeUpTimeUpdated = false;
+      Duration sleepInterval = Duration.ofMillis(this.wakeUpTime - Workflow.currentTimeMillis());
+      logger.info("Going to sleep for " + sleepInterval);
+      if (!Workflow.await(sleepInterval, () -> wakeUpTimeUpdated)) {
+        break;
+      }
+    } while (wakeUpTimeUpdated);
+    logger.info("sleepUntil completed");
+  }
+
+  public void updateWakeUpTime(long wakeUpTime) {
+    this.wakeUpTime = wakeUpTime;
+    this.wakeUpTimeUpdated = true;
+  }
+
+  public long getWakeUpTime() {
+    return wakeUpTime;
+  }
+}
---
+++ src/main/java/io/temporal/samples/updatabletimer/WakeUpTimeUpdater.java
@@ -0,0 +1,45 @@
+/*
+ *  Copyright (c) 2020 Temporal Technologies, Inc. All Rights Reserved
+ *
+ *  Copyright 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ *  use this file except in compliance with the License. A copy of the License is
+ *  located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ *  or in the "license" file accompanying this file. This file is distributed on
+ *  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+
+package io.temporal.samples.updatabletimer;
+
+import static io.temporal.samples.updatabletimer.DynamicSleepWorkflowWorker.DYNAMIC_SLEEP_WORKFLOW_ID;
+
+import io.temporal.client.WorkflowClient;
+import io.temporal.serviceclient.WorkflowServiceStubs;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WakeUpTimeUpdater {
+
+  private static final Logger logger = LoggerFactory.getLogger(WakeUpTimeUpdater.class);
+
+  public static void main(String[] args) {
+    WorkflowServiceStubs service = WorkflowServiceStubs.newInstance();
+    WorkflowClient client = WorkflowClient.newInstance(service);
+
+    // Create a stub that points to an existing workflow with the given ID
+    DynamicSleepWorkflow workflow =
+        client.newWorkflowStub(DynamicSleepWorkflow.class, DYNAMIC_SLEEP_WORKFLOW_ID);
+
+    // signal workflow about the wake up time change
+    workflow.updateWakeUpTime(System.currentTimeMillis() + 10000);
+    logger.info("Updated wake up time to 10 seconds from now");
+  }
+}
