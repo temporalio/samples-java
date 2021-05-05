4e00cacdaa95ccced68ab4b876ec8f2ab87015a3
Tihomir Surdilovic <tihomir@temporal.io>
2021-05-05 09:42:04 -0400

    Adding SideEffect example (#95)
    * Adding SideEffect example
    
    Signed-off-by: Tihomir Surdilovic <tihomir@temporal.io>
    
    * fix test - setting same workflowfactoryoptions as in example
    
    Signed-off-by: Tihomir Surdilovic <tihomir@temporal.io>
    
    * updates per review
    
    Signed-off-by: Tihomir Surdilovic <tihomir@temporal.io>
    
    * updated per review
    
    Signed-off-by: Tihomir Surdilovic <tihomir@temporal.io>
    
    * typo
    
    Signed-off-by: Tihomir Surdilovic <tihomir@temporal.io>
    
    * updates per comments
    
    Signed-off-by: Tihomir Surdilovic <tihomir@temporal.io>
    
    * formatting
    
    Signed-off-by: Tihomir Surdilovic <tihomir@temporal.io>

---
+++ src/test/java/io/temporal/samples/hello/HelloSideEffectTest.java
@@ -0,0 +1,77 @@
+/*
+ *  Copyright (c) 2020 Temporal Technologies, Inc. All Rights Reserved
+ *
+ *  Copyright 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ *  use this file except in compliance with the License. A copy of the License is
+ *  located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ *  or in the "license" file accompanying this file. This file is distributed on
+ *  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+
+package io.temporal.samples.hello;
+
+import static io.temporal.samples.hello.HelloActivity.TASK_QUEUE;
+import static org.junit.Assert.assertEquals;
+
+import io.temporal.client.WorkflowClient;
+import io.temporal.client.WorkflowOptions;
+import io.temporal.testing.TestEnvironmentOptions;
+import io.temporal.testing.TestWorkflowEnvironment;
+import io.temporal.worker.Worker;
+import io.temporal.worker.WorkerFactoryOptions;
+import java.time.Duration;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HelloSideEffectTest {
+  private TestWorkflowEnvironment testEnv;
+  private Worker worker;
+  private WorkflowClient client;
+
+  @Before
+  public void setUp() {
+    TestEnvironmentOptions testEnvironmentOptions =
+        TestEnvironmentOptions.newBuilder()
+            .setWorkerFactoryOptions(
+                WorkerFactoryOptions.newBuilder()
+                    .setWorkflowHostLocalTaskQueueScheduleToStartTimeout(Duration.ZERO)
+                    .build())
+            .build();
+
+    testEnv = TestWorkflowEnvironment.newInstance(testEnvironmentOptions);
+    worker = testEnv.newWorker(TASK_QUEUE);
+    worker.registerWorkflowImplementationTypes(HelloSideEffect.SideEffectWorkflowImpl.class);
+    client = testEnv.getWorkflowClient();
+  }
+
+  @After
+  public void tearDown() {
+    testEnv.close();
+  }
+
+  @Test
+  public void testSideffectsWorkflow() {
+    worker.registerActivitiesImplementations(new HelloSideEffect.SideEffectActivitiesImpl());
+    testEnv.start();
+
+    // Get a workflow stub using the same task queue the worker uses.
+    HelloSideEffect.SideEffectWorkflow workflow =
+        client.newWorkflowStub(
+            HelloSideEffect.SideEffectWorkflow.class,
+            WorkflowOptions.newBuilder().setTaskQueue(TASK_QUEUE).build());
+    // Execute a workflow waiting for it to complete.
+    String result = workflow.execute();
+    // make sure the result is same as the query result after workflow completion
+    assertEquals(result, workflow.getResult());
+  }
+}
