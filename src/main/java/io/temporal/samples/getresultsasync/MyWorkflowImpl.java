13af0c1e203943b70ebbfaba7902538b059df032
Tihomir Surdilovic <tihomir@temporal.io>
2021-09-20 18:10:14 -0400

    Sample showing use of getResult and getResultAsync (#165)
    * Sample showing use of getResult and getResultAsync
    
    Signed-off-by: Tihomir Surdilovic <tihomir@temporal.io>
    
    * renamed package name
    
    Signed-off-by: Tihomir Surdilovic <tihomir@temporal.io>
    
    * update readme and add test
    
    Signed-off-by: Tihomir Surdilovic <tihomir@temporal.io>

---
+++ src/main/java/io/temporal/samples/getresultsasync/MyWorkflowImpl.java
@@ -0,0 +1,31 @@
+/*
+ *  Copyright (c) 2020 Temporal Technologies, Inc. All Rights Reserved
+ *
+ *  Copyright 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ *  use this file except in compliance with the License. A copy of the License is
+ *  located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ *  or in the "license" file accompanying this file. This file is distributed on
+ *  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+
+package io.temporal.samples.getresultsasync;
+
+import io.temporal.workflow.Workflow;
+import java.time.Duration;
+
+public class MyWorkflowImpl implements MyWorkflow {
+  @Override
+  public String justSleep(int seconds) {
+    Workflow.sleep(Duration.ofSeconds(seconds));
+    return "woke up after " + seconds + " seconds";
+  }
+}
