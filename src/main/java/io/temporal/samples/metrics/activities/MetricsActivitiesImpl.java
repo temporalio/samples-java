f3a387e86f9a606eee5670b7ffdfcb75a6f95720
Tihomir Surdilovic <tihomir@temporal.io>
2022-01-31 22:23:37 -0500

    update to show custom counter metric creation
    Signed-off-by: Tihomir Surdilovic <tihomir@temporal.io>

--- src/main/java/io/temporal/samples/metrics/activities/MetricsActivitiesImpl.java
+++ src/main/java/io/temporal/samples/metrics/activities/MetricsActivitiesImpl.java
@@ -19,14 +19,32 @@
 
 package io.temporal.samples.metrics.activities;
 
+import io.temporal.activity.Activity;
+import io.temporal.activity.ActivityExecutionContext;
+
 public class MetricsActivitiesImpl implements MetricsActivities {
+
   @Override
   public String performA(String input) {
+    // simulate some random failures to trigger retries
+    if (Activity.getExecutionContext().getInfo().getAttempt() < 3) {
+      incRetriesCustomMetric(Activity.getExecutionContext());
+      throw Activity.wrap(new NullPointerException("simulated"));
+    }
     return "Performed activity A with input " + input + "\n";
   }
 
   @Override
   public String performB(String input) {
+    // simulate some random failures to trigger retries
+    if (Activity.getExecutionContext().getInfo().getAttempt() < 5) {
+      incRetriesCustomMetric(Activity.getExecutionContext());
+      throw Activity.wrap(new NullPointerException("simulated"));
+    }
     return "Performed activity B with input " + input + "\n";
+  }
+
+  private void incRetriesCustomMetric(ActivityExecutionContext context) {
+    context.getMetricsScope().counter("activity_retries").inc(1);
   }
 }
