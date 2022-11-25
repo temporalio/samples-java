54ddb30704db54b0f41272521ce67ddb563c0a0a
Maxim Fateev <mfateev@gmail.com>
2022-11-24 19:27:32 -0800

    Added missing WorkflowMethod annotation

--- src/main/java/io/temporal/samples/batch/slidingwindow/RecordProcessorWorkflow.java
+++ src/main/java/io/temporal/samples/batch/slidingwindow/RecordProcessorWorkflow.java
@@ -20,10 +20,12 @@
 package io.temporal.samples.batch.slidingwindow;
 
 import io.temporal.workflow.WorkflowInterface;
+import io.temporal.workflow.WorkflowMethod;
 
 @WorkflowInterface
 public interface RecordProcessorWorkflow {
 
   /** Processes a single record */
+  @WorkflowMethod
   void processRecord(Record r);
 }
