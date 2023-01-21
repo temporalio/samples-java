5966d5e0531845a0d38b8f9d5ba753a4a8705719
Maxim Fateev <mfateev@gmail.com>
2023-01-21 13:43:28 -0800

    Added comment per review feedback

--- src/main/java/io/temporal/samples/retryonsignalinterceptor/MyActivityImpl.java
+++ src/main/java/io/temporal/samples/retryonsignalinterceptor/MyActivityImpl.java
@@ -24,6 +24,10 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class MyActivityImpl implements MyActivity {
 
+  /**
+   * WARNING! Never rely on such shared state in real applications. The activity variables are per
+   * process and in almost all cases multiple worker processes are used.
+   */
   private final AtomicInteger count = new AtomicInteger();
 
   /** Sleeps 5 seconds. Fails for 4 first invocations, and then completes. */
