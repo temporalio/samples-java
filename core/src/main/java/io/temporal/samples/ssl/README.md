04401059b991316c281d093fad9006ca924f6850
Aymalla <Aymalla@outlook.com>
2023-06-20 16:06:12 +0100

    rebase to main branch

--- core/src/main/java/io/temporal/samples/ssl/README.md
+++ core/src/main/java/io/temporal/samples/ssl/README.md
@@ -1,11 +1,11 @@
 # Workflow execution with mTLS
 
-
 This example shows how to secure your Temporal application with [mTLS](https://docs.temporal.io/security/#encryption-in-transit-with-mtls).
 This is required to connect with Temporal Cloud or any production Temporal deployment.
 
 
 ## Export env variables
+
 Before running the example you need to export the following env variables: 
 
 - TEMPORAL_ENDPOINT: grpc endpoint, for Temporal Cloud would like `${namespace}.tmprl.cloud:7233`.
@@ -13,10 +13,48 @@ Before running the example you need to export the following env variables:
 - TEMPORAL_CLIENT_CERT: For Temporal Cloud see requirements [here](https://docs.temporal.io/cloud/how-to-manage-certificates-in-temporal-cloud#end-entity-certificates).
 - TEMPORAL_CLIENT_KEY: For Temporal Cloud see requirements [here](https://docs.temporal.io/cloud/how-to-manage-certificates-in-temporal-cloud#end-entity-certificates).
 
-
-
 ## Running this sample
 
 ```bash
 ./gradlew -q execute -PmainClass=io.temporal.samples.ssl.Starter
+```
+
+## Run SslEnabledWorkerCustomCA Sample
+
+This sample shows how to start a worker that connects to a temporal cluster with mTLS enabled; created by ([tls-simple sample](https://github.com/temporalio/samples-server/tree/main/tls/tls-simple));
+
+SslEnabledWorkerCustomCA demonstrates:
+
+- Passing a custom CA certificate file as parameter
+- Overriding the authority name used for TLS handshakes (if needed)
+
+1.Start a temporal cluster with tls
+
+Please follow the temporal server-sample to start simple Temporal mTLS cluster locally: [tls-simple](https://github.com/temporalio/samples-server/tree/main/tls/tls-simple)
+
+2.Set environment variables
+
+```bash
+# Environment variables
+# paths to ca cert, client cert and client key come from the previous step 
+export TEMPORAL_CLIENT_CERT="</path/to/client.pem>"
+export TEMPORAL_CLIENT_KEY="</path/to/client.key>"
+export TEMPORAL_CA_CERT="</path/to/ca.cert>"    
+export TEMPORAL_ENDPOINT="localhost:7233"    # Temporal grpc endpoint       
+export TEMPORAL_NAMESPACE="default"          # Temporal namespace            
+export TEMPORAL_SERVER_HOSTNAME="tls-sample" # Temporal server host name  
+```
+
+3.Start the Worker
+
+```bash
+gradle -q execute -PmainClass=io.temporal.samples.ssl.SslEnabledWorkerCustomCA
+```
+
+4.Expected result
+
+```text
+[main] INFO  i.t.s.WorkflowServiceStubsImpl - Created WorkflowServiceStubs for channel: ManagedChannelOrphanWrapper{delegate=ManagedChannelImpl{logId=1, target=localhost:7233}} 
+[main] INFO  io.temporal.internal.worker.Poller - start: Poller{name=Workflow Poller taskQueue="MyTaskQueue", namespace="default"} 
+Workflow completed:done 
 ```
--- core/src/main/java/io/temporal/samples/ssl/SslEnabledWorkerCustomCA.java
+++ core/src/main/java/io/temporal/samples/ssl/SslEnabledWorkerCustomCA.java
@@ -24,8 +24,10 @@ import io.grpc.netty.shaded.io.netty.handler.ssl.SslContext;
 import io.grpc.netty.shaded.io.netty.handler.ssl.SslContextBuilder;
 import io.temporal.client.WorkflowClient;
 import io.temporal.client.WorkflowClientOptions;
+import io.temporal.client.WorkflowOptions;
 import io.temporal.serviceclient.WorkflowServiceStubs;
 import io.temporal.serviceclient.WorkflowServiceStubsOptions;
+import io.temporal.worker.Worker;
 import io.temporal.worker.WorkerFactory;
 import java.io.FileInputStream;
 import java.io.InputStream;
@@ -36,13 +38,22 @@ public class SslEnabledWorkerCustomCA {
 
   public static void main(String[] args) throws Exception {
 
-    // Load your client certificate
+    // Load your client certificate, which should look like:
+    // -----BEGIN CERTIFICATE-----
+    // ...
+    // -----END CERTIFICATE-----
     InputStream clientCert = new FileInputStream(System.getenv("TEMPORAL_CLIENT_CERT"));
 
-    // PKCS8 client key
+    // PKCS8 client key, which should look like:
+    // -----BEGIN PRIVATE KEY-----
+    // ...
+    // -----END PRIVATE KEY-----
     InputStream clientKey = new FileInputStream(System.getenv("TEMPORAL_CLIENT_KEY"));
 
-    // Certification Authority certificate
+    // Load your Certification Authority certificate, which should look like:
+    // -----BEGIN CERTIFICATE-----
+    // ...
+    // -----END CERTIFICATE-----
     InputStream caCert = new FileInputStream(System.getenv("TEMPORAL_CA_CERT"));
 
     // For temporal cloud this would likely be ${namespace}.tmprl.cloud:7233
@@ -74,9 +85,7 @@ public class SslEnabledWorkerCustomCA {
                 .build());
 
     // Now setup and start workflow worker, which uses SSL enabled gRPC service to
-    // communicate with
-    // backend.
-    // client that can be used to start and signal workflows.
+    // communicate with backend. client that can be used to start and signal workflows.
     WorkflowClient client =
         WorkflowClient.newInstance(
             service, WorkflowClientOptions.newBuilder().setNamespace(namespace).build());
@@ -84,13 +93,42 @@ public class SslEnabledWorkerCustomCA {
     // worker factory that can be used to create workers for specific task queues
     WorkerFactory factory = WorkerFactory.newInstance(client);
 
-    // Worker that listens on a task queue and hosts both workflow and activity
-    // implementations.
-    factory.newWorker(TASK_QUEUE);
+    /*
+     * Define the workflow worker. Workflow workers listen to a defined task queue and process
+     * workflows and activities.
+     */
+    Worker worker = factory.newWorker(TASK_QUEUE);
 
-    // TODO: now register your workflow types and activity implementations.
-    // worker.registerWorkflowImplementationTypes(...);
-    // worker.registerActivitiesImplementations(...);
+    /*
+     * Register our workflow implementation with the worker.
+     * Workflow implementations must be known to the worker at runtime in
+     * order to dispatch workflow tasks.
+     */
+    worker.registerWorkflowImplementationTypes(MyWorkflowImpl.class);
+
+    /*
+     * Start all the workers registered for a specific task queue.
+     * The started workers then start polling for workflows and activities.
+     */
     factory.start();
+
+    // Create the workflow client stub. It is used to start our workflow execution.
+    MyWorkflow workflow =
+        client.newWorkflowStub(
+            MyWorkflow.class,
+            WorkflowOptions.newBuilder()
+                .setWorkflowId("WORKFLOW_ID")
+                .setTaskQueue(TASK_QUEUE)
+                .build());
+
+    /*
+     * Execute our workflow and wait for it to complete. The call to our execute method is
+     * synchronous.
+     */
+    String greeting = workflow.execute();
+
+    // Display workflow execution results
+    System.out.println("Workflow completed:" + greeting);
+    System.exit(0);
   }
 }
