159c7b8bfe36b1d8c32d2813089a47bd569c7008
Quinn Klassen <klassenq@gmail.com>
2025-02-17 22:14:41 -0800

    Update Nexus Sample for v1.28.0

--- core/src/main/java/io/temporal/samples/nexus/handler/NexusServiceImpl.java
+++ core/src/main/java/io/temporal/samples/nexus/handler/NexusServiceImpl.java
@@ -34,12 +34,12 @@ import io.temporal.samples.nexus.service.NexusService;
 public class NexusServiceImpl {
   @OperationImpl
   public OperationHandler<NexusService.EchoInput, NexusService.EchoOutput> echo() {
-    // OperationHandler.sync is a meant for exposing simple RPC handlers.
+    // WorkflowClientOperationHandlers.sync is a meant for exposing simple RPC handlers.
     return OperationHandler.sync(
-        // The method is for making arbitrary short calls to other services or databases, or
-        // perform simple computations such as this one. Users can also access a workflow client by
-        // calling
-        // Nexus.getOperationContext().getWorkflowClient(ctx) to make arbitrary calls such as
+        // The method is for to make arbitrary short calls to other services or databases, or
+        // perform simple computations such as this one.
+        // Users can also access a client by calling
+        // Nexus.getOperationContext().getWorkflowClient(ctx) to mae arbitrary calls such as
         // signaling, querying, or listing workflows.
         (ctx, details, input) -> new NexusService.EchoOutput(input.getMessage()));
   }
