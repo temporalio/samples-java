e8a6c1d307620bcb24c5d7faa4eabefdc6b97f05
Quinn Klassen <klassenq@gmail.com>
2025-02-28 15:02:04 -0800

    Add interceptor to propagate headers to Nexus operations

---
+++ core/src/main/java/io/temporal/samples/nexuscontextpropogation/caller/CallerStarter.java
@@ -0,0 +1,64 @@
+/*
+ *  Copyright (c) 2020 Temporal Technologies, Inc. All Rights Reserved
+ *
+ *  Copyright 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ *  use this file except in compliance with the License. A copy of the License is
+ *  located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ *  or in the "license" file accompanying this file. This file is distributed on
+ *  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+
+package io.temporal.samples.nexuscontextpropogation.caller;
+
+import io.temporal.api.common.v1.WorkflowExecution;
+import io.temporal.client.WorkflowClient;
+import io.temporal.client.WorkflowClientOptions;
+import io.temporal.client.WorkflowOptions;
+import io.temporal.samples.nexus.caller.EchoCallerWorkflow;
+import io.temporal.samples.nexus.caller.HelloCallerWorkflow;
+import io.temporal.samples.nexus.options.ClientOptions;
+import io.temporal.samples.nexus.service.NexusService;
+import io.temporal.samples.nexuscontextpropogation.propogation.MDCContextPropagator;
+import java.util.Collections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CallerStarter {
+  private static final Logger logger = LoggerFactory.getLogger(CallerStarter.class);
+
+  public static void main(String[] args) {
+    WorkflowClient client =
+        ClientOptions.getWorkflowClient(
+            args,
+            WorkflowClientOptions.newBuilder()
+                .setContextPropagators(Collections.singletonList(new MDCContextPropagator())));
+
+    WorkflowOptions workflowOptions =
+        WorkflowOptions.newBuilder().setTaskQueue(CallerWorker.DEFAULT_TASK_QUEUE_NAME).build();
+    EchoCallerWorkflow echoWorkflow =
+        client.newWorkflowStub(EchoCallerWorkflow.class, workflowOptions);
+    WorkflowExecution execution = WorkflowClient.start(echoWorkflow::echo, "Nexus Echo 👋");
+    logger.info(
+        "Started EchoCallerWorkflow workflowId: {} runId: {}",
+        execution.getWorkflowId(),
+        execution.getRunId());
+    logger.info("Workflow result: {}", echoWorkflow.echo("Nexus Echo 👋"));
+    HelloCallerWorkflow helloWorkflow =
+        client.newWorkflowStub(HelloCallerWorkflow.class, workflowOptions);
+    execution = WorkflowClient.start(helloWorkflow::hello, "Nexus", NexusService.Language.EN);
+    logger.info(
+        "Started HelloCallerWorkflow workflowId: {} runId: {}",
+        execution.getWorkflowId(),
+        execution.getRunId());
+    logger.info("Workflow result: {}", helloWorkflow.hello("Nexus", NexusService.Language.ES));
+  }
+}
---
+++ core/src/main/java/io/temporal/samples/nexuscontextpropogation/propogation/NexusMDCContextInterceptor.java
@@ -0,0 +1,126 @@
+/*
+ *  Copyright (c) 2020 Temporal Technologies, Inc. All Rights Reserved
+ *
+ *  Copyright 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ *  use this file except in compliance with the License. A copy of the License is
+ *  located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ *  or in the "license" file accompanying this file. This file is distributed on
+ *  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+
+package io.temporal.samples.nexuscontextpropogation.propogation;
+
+import io.nexusrpc.OperationException;
+import io.nexusrpc.handler.OperationContext;
+import io.temporal.common.interceptors.NexusOperationInboundCallsInterceptor;
+import io.temporal.common.interceptors.WorkerInterceptorBase;
+import io.temporal.common.interceptors.WorkflowInboundCallsInterceptor;
+import io.temporal.common.interceptors.WorkflowOutboundCallsInterceptor;
+import java.util.Map;
+import org.slf4j.MDC;
+
+/** Propagates MDC context from the caller workflow to the Nexus service through the headers. */
+public class NexusMDCContextInterceptor extends WorkerInterceptorBase {
+  private static final String NEXUS_HEADER_PREFIX = "x-nexus-";
+
+  @Override
+  public WorkflowInboundCallsInterceptor interceptWorkflow(WorkflowInboundCallsInterceptor next) {
+    return new WorkflowInboundCallsInterceptorNexusMDC(next);
+  }
+
+  public static class WorkflowInboundCallsInterceptorNexusMDC
+      extends io.temporal.common.interceptors.WorkflowInboundCallsInterceptorBase {
+    private final WorkflowInboundCallsInterceptor next;
+
+    public WorkflowInboundCallsInterceptorNexusMDC(WorkflowInboundCallsInterceptor next) {
+      super(next);
+      this.next = next;
+    }
+
+    @Override
+    public void init(WorkflowOutboundCallsInterceptor outboundCalls) {
+      next.init(new WorkflowOutboundCallsInterceptorNexusMDC(outboundCalls));
+    }
+  }
+
+  public static class WorkflowOutboundCallsInterceptorNexusMDC
+      extends io.temporal.common.interceptors.WorkflowOutboundCallsInterceptorBase {
+    private final WorkflowOutboundCallsInterceptor next;
+
+    public WorkflowOutboundCallsInterceptorNexusMDC(WorkflowOutboundCallsInterceptor next) {
+      super(next);
+      this.next = next;
+    }
+
+    @Override
+    public <R> ExecuteNexusOperationOutput<R> executeNexusOperation(
+        ExecuteNexusOperationInput<R> input) {
+      Map<String, String> contextMap = MDC.getCopyOfContextMap();
+      if (contextMap != null) {
+        Map<String, String> headerMap = input.getHeaders();
+        contextMap.forEach(
+            (k, v) -> {
+              if (k.startsWith(NEXUS_HEADER_PREFIX)) {
+                headerMap.put(k, v);
+              }
+            });
+      }
+      return next.executeNexusOperation(input);
+    }
+  }
+
+  @Override
+  public NexusOperationInboundCallsInterceptor interceptNexusOperation(
+      OperationContext context, NexusOperationInboundCallsInterceptor next) {
+    return new NexusOperationInboundCallsInterceptorNexusMDC(next);
+  }
+
+  private static class NexusOperationInboundCallsInterceptorNexusMDC
+      extends io.temporal.common.interceptors.NexusOperationInboundCallsInterceptorBase {
+    private final NexusOperationInboundCallsInterceptor next;
+
+    public NexusOperationInboundCallsInterceptorNexusMDC(
+        NexusOperationInboundCallsInterceptor next) {
+      super(next);
+      this.next = next;
+    }
+
+    @Override
+    public StartOperationOutput startOperation(StartOperationInput input)
+        throws OperationException {
+      input
+          .getOperationContext()
+          .getHeaders()
+          .forEach(
+              (k, v) -> {
+                if (k.startsWith(NEXUS_HEADER_PREFIX)) {
+                  MDC.put(k, v);
+                }
+              });
+      return next.startOperation(input);
+    }
+
+    @Override
+    public CancelOperationOutput cancelOperation(CancelOperationInput input) {
+      input
+          .getOperationContext()
+          .getHeaders()
+          .forEach(
+              (k, v) -> {
+                if (k.startsWith(NEXUS_HEADER_PREFIX)) {
+                  MDC.put(k, v);
+                }
+              });
+      return next.cancelOperation(input);
+    }
+  }
+}
