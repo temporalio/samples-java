e798802ef6919ec5ef7a498689e8a92d5f98f172
Tihomir Surdilovic <tihomir@temporal.io>
2025-07-01 11:57:01 -0400

    updates per recommendations
    Signed-off-by: Tihomir Surdilovic <tihomir@temporal.io>

--- core/src/main/java/io/temporal/samples/autoheartbeat/README.md
+++ core/src/main/java/io/temporal/samples/autoheartbeat/README.md
@@ -9,6 +9,11 @@ but then at times can take for example minutes. In this case you have to set lon
 but you might not want first heartbeat to be sent right away but send it after the "shorter" duration of activity
 execution. 
 
+Warning: make sure to test this sample for your use case. This includes load testing. This sample was not 
+tested on large scale workloads. In addition note that it is recommended to heartbeat from activity code itself. Using
+this type of autoheartbeating utility does have disatvantage that activity code itself can continue running after 
+a handled activity cancelation. Please be aware of these warnings when applying this sample.
+
 1. Start the Sample:
 
 ```bash
--- core/src/main/java/io/temporal/samples/autoheartbeat/Starter.java
+++ core/src/main/java/io/temporal/samples/autoheartbeat/Starter.java
@@ -22,6 +22,7 @@ package io.temporal.samples.autoheartbeat;
 import io.temporal.client.WorkflowClient;
 import io.temporal.client.WorkflowOptions;
 import io.temporal.client.WorkflowStub;
+import io.temporal.failure.CanceledFailure;
 import io.temporal.samples.autoheartbeat.activities.AutoActivitiesImpl;
 import io.temporal.samples.autoheartbeat.interceptor.AutoHeartbeatWorkerInterceptor;
 import io.temporal.samples.autoheartbeat.workflows.AutoWorkflow;
@@ -32,90 +33,135 @@ import io.temporal.worker.WorkerFactory;
 import io.temporal.worker.WorkerFactoryOptions;
 
 public class Starter {
-  static final String TASK_QUEUE = "AutoheartbeatTaskQueue";
-  static final String WORKFLOW_ID = "AutoHeartbeatWorkflow";
+    static final String TASK_QUEUE = "AutoheartbeatTaskQueue";
+    static final String WORKFLOW_ID = "AutoHeartbeatWorkflow";
 
-  public static void main(String[] args) {
-    WorkflowServiceStubs service = WorkflowServiceStubs.newLocalServiceStubs();
-    WorkflowClient client = WorkflowClient.newInstance(service);
+    public static void main(String[] args) {
+        WorkflowServiceStubs service = WorkflowServiceStubs.newLocalServiceStubs();
+        WorkflowClient client = WorkflowClient.newInstance(service);
 
-    // Configure our auto heartbeat workflow interceptor which will apply
-    // AutoHeartbeaterUtil to each activity workflow schedules which has a heartbeat
-    // timeout configured
-    WorkerFactoryOptions wfo =
-        WorkerFactoryOptions.newBuilder()
-            .setWorkerInterceptors(new AutoHeartbeatWorkerInterceptor())
-            .build();
+        // Configure our auto heartbeat workflow interceptor which will apply
+        // AutoHeartbeaterUtil to each activity workflow schedules which has a heartbeat
+        // timeout configured
+        WorkerFactoryOptions wfo =
+                WorkerFactoryOptions.newBuilder()
+                        .setWorkerInterceptors(new AutoHeartbeatWorkerInterceptor())
+                        .build();
 
-    WorkerFactory factory = WorkerFactory.newInstance(client, wfo);
-    Worker worker = factory.newWorker(TASK_QUEUE);
+        WorkerFactory factory = WorkerFactory.newInstance(client, wfo);
+        Worker worker = factory.newWorker(TASK_QUEUE);
 
-    worker.registerWorkflowImplementationTypes(AutoWorkflowImpl.class);
-    worker.registerActivitiesImplementations(new AutoActivitiesImpl());
+        worker.registerWorkflowImplementationTypes(AutoWorkflowImpl.class);
+        worker.registerActivitiesImplementations(new AutoActivitiesImpl());
 
-    factory.start();
+        factory.start();
 
-    System.out.println("**** First Run: run workflow to completion");
+        // first run completes execution with autoheartbeat utils
+        firstRun(client);
+        // second run cancels running (pending) activity via signal (specific scope cancel)
+        secondRun(client);
+        // third run cancels running execution which cancels activity as well
+        thirdRun(client);
+        // fourth run turns off autoheartbeat for activities and lets activity time out on heartbeat
+        // timeout
+        fourthRun(client);
 
-    AutoWorkflow firstRun =
-        client.newWorkflowStub(
-            AutoWorkflow.class,
-            WorkflowOptions.newBuilder()
-                .setWorkflowId(WORKFLOW_ID)
-                .setTaskQueue(TASK_QUEUE)
-                .build());
-
-    try {
-      String firstRunResult = firstRun.exec("Auto heartbeating is cool");
-      System.out.println("First run result: " + firstRunResult);
-    } catch (Exception e) {
-      System.out.println("First run - Workflow exec exception: " + e.getClass().getName());
+        System.exit(0);
     }
 
-    System.out.println("\n\n**** Second Run: cancel activities");
+    @SuppressWarnings("unused")
+    private static void firstRun(WorkflowClient client) {
+        System.out.println("**** First Run: run workflow to completion");
+        AutoWorkflow firstRun =
+                client.newWorkflowStub(
+                        AutoWorkflow.class,
+                        WorkflowOptions.newBuilder()
+                                .setWorkflowId(WORKFLOW_ID)
+                                .setTaskQueue(TASK_QUEUE)
+                                .build());
 
-    AutoWorkflow secondRun =
-        client.newWorkflowStub(
-            AutoWorkflow.class,
-            WorkflowOptions.newBuilder()
-                .setWorkflowId(WORKFLOW_ID)
-                .setTaskQueue(TASK_QUEUE)
-                .build());
-    WorkflowClient.start(secondRun::exec, "Auto heartbeating is cool");
-    doSleeps(4);
-    secondRun.cancelActivity();
-
-    try {
-      String secondRunResult = WorkflowStub.fromTyped(secondRun).getResult(String.class);
-      System.out.println("Second run result: " + secondRunResult);
-    } catch (Exception e) {
-      System.out.println("Second run - Workflow exec exception: " + e.getClass().getName());
+        try {
+            String firstRunResult = firstRun.exec("Auto heartbeating is cool");
+            System.out.println("First run result: " + firstRunResult);
+        } catch (Exception e) {
+            System.out.println("First run - Workflow exec exception: " + e.getClass().getName());
+        }
     }
 
-    System.out.println("\n\n**** Third Run: cause heartbeat timeout");
-    // we disable autoheartbeat via env var
-    System.setProperty("sample.disableAutoHeartbeat", "true");
-    AutoWorkflow thirdRun =
-        client.newWorkflowStub(
-            AutoWorkflow.class,
-            WorkflowOptions.newBuilder()
-                .setWorkflowId(WORKFLOW_ID)
-                .setTaskQueue(TASK_QUEUE)
-                .build());
+    @SuppressWarnings("unused")
+    private static void secondRun(WorkflowClient client) {
+        System.out.println("\n\n**** Second Run: cancel activities via signal");
+        AutoWorkflow secondRun =
+                client.newWorkflowStub(
+                        AutoWorkflow.class,
+                        WorkflowOptions.newBuilder()
+                                .setWorkflowId(WORKFLOW_ID)
+                                .setTaskQueue(TASK_QUEUE)
+                                .build());
+        WorkflowClient.start(secondRun::exec, "Auto heartbeating is cool");
+        doSleeps(4);
+        secondRun.cancelActivity();
 
-    try {
-      String thirdRunResult = thirdRun.exec("Auto heartbeating is cool");
-      System.out.println("Third run result: " + thirdRunResult);
-    } catch (Exception e) {
-      System.out.println("Third run - Workflow exec exception: " + e.getClass().getName());
+        try {
+            String secondRunResult = WorkflowStub.fromTyped(secondRun).getResult(String.class);
+            System.out.println("Second run result: " + secondRunResult);
+        } catch (Exception e) {
+            System.out.println("Second run - Workflow exec exception: " + e.getClass().getName());
+        }
     }
-  }
 
-  private static void doSleeps(int seconds) {
-    try {
-      Thread.sleep(seconds * 1000L);
-    } catch (Exception e) {
-      System.out.println(e.getMessage());
+    @SuppressWarnings("unused")
+    private static void thirdRun(WorkflowClient client) {
+        System.out.println("\n\n**** Third Run: cancel workflow execution");
+        AutoWorkflow thirdRun =
+                client.newWorkflowStub(
+                        AutoWorkflow.class,
+                        WorkflowOptions.newBuilder()
+                                .setWorkflowId(WORKFLOW_ID)
+                                .setTaskQueue(TASK_QUEUE)
+                                .build());
+        WorkflowClient.start(thirdRun::exec, "Auto heartbeating is cool");
+        doSleeps(10);
+        try {
+            WorkflowStub.fromTyped(thirdRun).cancel();
+            String thirdRunResult = WorkflowStub.fromTyped(thirdRun).getResult(String.class);
+            System.out.println("Third run result: " + thirdRunResult);
+        } catch (Exception e) {
+            // we are expecting workflow cancelation
+            if (e.getCause() instanceof CanceledFailure) {
+                System.out.println("Third run - Workflow execution canceled.");
+            } else {
+                System.out.println("Third run - Workflow exec exception: " + e.getMessage());
+            }
+        }
     }
-  }
+
+    @SuppressWarnings("unused")
+    private static void fourthRun(WorkflowClient client) {
+        System.out.println("\n\n**** Fourth Run: cause heartbeat timeout");
+        // we disable autoheartbeat via env var
+        System.setProperty("sample.disableAutoHeartbeat", "true");
+        AutoWorkflow fourth =
+                client.newWorkflowStub(
+                        AutoWorkflow.class,
+                        WorkflowOptions.newBuilder()
+                                .setWorkflowId(WORKFLOW_ID)
+                                .setTaskQueue(TASK_QUEUE)
+                                .build());
+
+        try {
+            String fourthRunResult = fourth.exec("Auto heartbeating is cool");
+            System.out.println("Fourth run result: " + fourthRunResult);
+        } catch (Exception e) {
+            System.out.println("Fourth run - Workflow exec exception: " + e.getClass().getName());
+        }
+    }
+
+    private static void doSleeps(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000L);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
